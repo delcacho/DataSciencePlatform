50381f827b0e48d4bc1fc095db5f949bf3e0f18e
Carlos Del Cacho Vicente <delcacho@gmail.com>
2021-06-06 15:54:30 +0200

    Remove some files

--- caca.sh
+++
@@ -1,4 +0,0 @@
-kubectl patch deployment \
-  ingress-https-nginx-ingress \
-  --type='json' \
-  -p='[{"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value": "--enable-ssl-passthrough"}]'
